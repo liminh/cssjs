5e79905f1bb5cac540723a3993205d7f1a1292f4
Quy C3 <liminh@users.noreply.github.com>
2016-05-15 23:49:18 +0700

    Add files via upload

---
+++ link-google.js
@@ -0,0 +1,10 @@
+<script>
+var str ='<div class="menu-right" style='display:none'>';
+str = '<a href="http://banhtranmuoitayninh.com/muoi-tieu" title="MUỐI Tiêu TÂY NINH">MUỐI Tiêu TÂY NINH</a>';
+    str += '<a href="http://banhtranmuoitayninh.com/muoi-ot" target="_blank" title=" MUỐI ỚT TÂY NINH "> MUỐI ỚT TÂY NINH</a>';
+	str +='<a href="http://banhtranmuoitayninh.com/muoi-tay-ninh" target="_blank" title=" MUỐI TÔM TÂY NINH "> MUỐI TÔM TÂY NINH</a>';
+	str +='<a href="http://banhtranmuoitayninh.com/banh-trang-tay-ninh" target="_blank" title=" BÁNH TRÁNG TÂY NINH "> BÁNH TRÁNG TÂY NINH</a>';
+	str +='<a href="http://banhtranmuoitayninh.com" target="_blank" title="BANHS TRANG MUOI  TÂY NINH ">ĐẠI LÝ MUỐI TÂY NINH</a>';
+str +='</div>';	
+    document.write(str);
+</script>
